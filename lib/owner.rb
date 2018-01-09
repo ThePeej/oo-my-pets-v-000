4baa91748c8eff8688557e1b0b91ffc4e01a88aa
PJ Frias <peejfrias@gmail.com>
2018-01-09 21:48:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -74,9 +74,6 @@ class Owner
   ### sell/list pets methods ###
 
   def sell_pets
-    # self.pets[:dogs].each {|pet| pet.mood = "nervous"}
-    # self.pets[:cats].each {|pet| pet.mood = "nervous"}
-    # self.pets[:fishes].each {|pet| pet.mood = "nervous"}
     self.pets.each do |species, pets|
       pets.each {|pet| pet.mood = "nervous"}
     end
